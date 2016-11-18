4833e34aa35d7733a8ddb446014e0204b5d5c45a
Cain Piper <c_piper@internode.on.net>
2016-11-18 15:49:02 +1100

    Changed all oscillator shapes to use tables.

--- SharpSynth/Oscillator.cs
+++ SharpSynth/Oscillator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SharpSynth
 {
@@ -13,17 +14,35 @@ namespace SharpSynth
 
 	public class Oscillator : SynthComponent
 	{
-		private const int TableBits = 18;
+		private const int TableBits = 4;
 		private const int TableSize = 1 << TableBits;
-		private static readonly float[] sinTable;
+		private const int TableMask = TableSize - 1;
+
+		private static readonly Dictionary<OscillatorShape, float[]> tables = new Dictionary<OscillatorShape, float[]>();
 
 		static Oscillator()
 		{
-			sinTable = new float[TableSize];
-			for (int i = 0; i < TableSize; i++)
+			var sinTable = new float[TableSize];
+			var squareTable = new float[TableSize];
+			var rampTable = new float[TableSize];
+			var sawTable = new float[TableSize];
+			var triangleTable = new float[TableSize];
+
+			for (var i = 0; i < TableSize; i++)
 			{
 				sinTable[i] = (float)Math.Sin(2 * Math.PI * i / TableSize);
+				squareTable[i] = 2 * (1f - (i >> (TableBits - 1))) - 1;
+				rampTable[i] = 2f * i / (TableSize-1) - 1;
+				sawTable[i] = -rampTable[i];
+				var result = 2f * ((i * 2) & TableMask) / TableSize - 1;
+				triangleTable[i] = i * 2 >= TableSize ? -result : result;
 			}
+
+			tables[OscillatorShape.Sin] = sinTable;
+			tables[OscillatorShape.Square] = squareTable;
+			tables[OscillatorShape.Ramp] = rampTable;
+			tables[OscillatorShape.Saw] = sawTable;
+			tables[OscillatorShape.Triangle] = triangleTable;
 		}
 
 		private double angle;
@@ -63,6 +82,16 @@ namespace SharpSynth
 		/// <param name="timeBase">The time base for the new samples. This value is in samples, which is measured at 44100 samples per second.</param>
 		protected override void GenerateSamples(float[] buffer, int count, long timeBase)
 		{
+			const double angleInc = TableSize / 44100.0;
+
+			float[] table;
+			if (!tables.TryGetValue(Shape, out table))
+			{
+				for (var i = 0; i < count; i++)
+					buffer[i] = 0;
+				return;
+			}
+
 			var frequency = Frequency.GenerateSamples(count, timeBase);
 			var level = Level.GenerateSamples(count, timeBase);
 			var scale = Scale.GenerateSamples(count, timeBase);
@@ -70,35 +99,14 @@ namespace SharpSynth
 
 			for (var i = 0; i < count; i++)
 			{
-				buffer[i] = level[i] + GenerateSample((int)(phaseOffset[i] * TableSize + angle) & (TableSize - 1)) * scale[i];
-				angle += frequency[i] * TableSize / 44100.0;
+				buffer[i] = level[i] + table[(int)(phaseOffset[i] * TableSize + angle) & TableMask] * scale[i];
+				angle += frequency[i] * angleInc;
 				if (angle > TableSize)
 					angle -= TableSize;
 			}
 
 			while (angle > TableSize)
 				angle -= TableSize;
-		}
-
-		public float GenerateSample(int a)
-		{
-			switch (Shape)
-			{
-				case OscillatorShape.Sin:
-					return sinTable[a];
-				case OscillatorShape.Square:
-					return 2 * (1f - (a >> (TableBits - 1))) - 1;
-				case OscillatorShape.Ramp:
-					return (2f * a / TableSize) - 1;
-				case OscillatorShape.Saw:
-					return -((2f * a / TableSize) - 1);
-				case OscillatorShape.Triangle:
-					a *= 2;
-					var result = ((2f * (a & (TableSize - 1)) / TableSize) - 1);
-					return (a >= TableSize) ? -result : result;
-				default:
-					return 0;
-			}
 		}
 
 		#endregion
