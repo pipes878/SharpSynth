873ec9db5b6c3ffe8228bd9ac2375648808c8f42
Cain <Cain>
2016-12-14 22:59:04 +1100

    Started on a BBD style delay.

--- SharpSynth/Delay.cs
+++ SharpSynth/Delay.cs
@@ -3,6 +3,67 @@ using SharpSynth.Input;
 
 namespace SharpSynth
 {
+	public class BbdDelay : SynthComponent
+	{
+		private const float minimumDelay = 0.2f;
+		private const float maximumDelay = 1f;
+		private float lastBufferedSample;
+		private float head;
+		private float[] bbdBuffer;
+
+		public ISynthComponent DelayTime { get; set; } = FixedValue.One;
+
+		public ISynthComponent Mix { get; set; } = FixedValue.Half;
+
+		public ISynthComponent Feedback { get; set; } = FixedValue.Half;
+
+		public ISynthComponent Input { get; set; }
+
+		public BbdDelay()
+		{
+			// This gives 1 second of day with the slowest clock.
+
+			bbdBuffer = new float[SampleRate];
+		}
+
+		protected override void GenerateSamples(float[] buffer, int count, long timeBase)
+		{
+			if (Input == null)
+			{
+				for (var i = 0; i < buffer.Length; i++)
+					buffer[i] = 0;
+				return;
+			}
+
+			var delay = DelayTime.GenerateSamples(count, timeBase);
+			var mix = Mix.GenerateSamples(count, timeBase);
+			var feedback = Feedback.GenerateSamples(count, timeBase);
+			var input = Input.GenerateSamples(count, timeBase);
+
+			for (var i = 0; i < count; i++)
+			{
+				var d = Math.Min(maximumDelay, Math.Max(minimumDelay, delay[i]));
+				var m = Math.Min(1, Math.Max(0, mix[i]));
+				var f = Math.Min(1, Math.Max(0, feedback[i]));
+
+				var s = ReplaceDelayedSample(input[i], d, f);
+			}
+		}
+
+		private float ReplaceDelayedSample(float input, float delay, float feedback)
+		{
+			// This works because the BBD is 1 second long, in samples.
+
+			var ticksPerSample = 1 / delay;
+
+			// First read the output of the BBD.
+
+			//bbdBuffer[head]
+
+			return 0;
+		}
+	}
+
 	public class Delay : SynthComponent
 	{
 		private int head;
